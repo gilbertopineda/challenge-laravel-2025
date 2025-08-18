204ddff14526b7a8b4aaf379178037de4354194d
Gilberto <gilberto.pineda@gmail.com>
2025-08-17 22:52:32 -0500

    feature: create classes to handle business logic

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\OrderRepository;
+use App\Repositories\OrderRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(OrderRepository::class, OrderRepositoryInterface::class);
     }
 
     /**
---
+++ app/Repositories/ItemRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories;
+
+class ItemRepositoryInterface
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
